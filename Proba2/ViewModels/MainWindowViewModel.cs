20dc565e5ef40ecb3f3ecdcc1a0add8a8f37a040
Aleksej Sinchuk <aleksejsincuk@gmail.com>
2019-06-29 10:36:19 +0300

    MyMVVM

---
+++ Proba2/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Proba2.ViewModels
+{
+    class MainWindowViewModel : Base.ViewModel
+    {
+        public string BG { set; get; }
+        public string bg
+        {
+            get
+            {
+                return BG;
+            }
+            set
+            {
+                BG = value;
+                OnPropertyChanged(nameof(BG));
+            }
+        }
+        public string Text { set; get; }
+        public Command Click { set; get; }
+       public string text
+        {
+            get
+            {
+                return Text;
+            }
+            set
+            {
+               
+                Text= value ;
+                OnPropertyChanged(nameof(Text));
+            }
+
+        }
+        public MainWindowViewModel()
+        {
+            Click = new Command(
+                () =>
+                {
+                    text = "QWEREWQWEREWQ";
+                    bg = "red";
+                }
+                );
+        }
+    }
+}
